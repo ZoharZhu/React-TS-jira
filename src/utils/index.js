244f3a2a9f85580ec13482ace4e87c69634ee02f
Zzh-Zh3nDu1 <530178528@qq.com>
2022-04-16 20:27:21 +0800

    feat: 添加useMount和useDebounce

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 // 在一个函数里，改变传入的对象本身是不好的
 export const cleanObject = (object) => {
@@ -8,4 +10,37 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+// export const debounce = (func, delay) => {
+//   let timer;
+//   return function (...args) {
+//     if (timer) {
+//       clearTimeout(timer);
+//     }
+//     timer = setTimeout(() => {
+//       func.apply(this, args);
+//     }, delay);
+//   };
+// }
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // 每次在value变化以后 设置一个定时器
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    // 每次在上一个useEffect处理完以后再下一个useEffect中运行（做一些清理工作）
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
 };
