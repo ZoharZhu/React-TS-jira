5a473a4ad1083f98b1968f2a94b9d77451ae6310
Zzh-Zh3nDu1 <530178528@qq.com>
2022-04-19 10:56:29 +0800

    feat: login和middleware

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { ProjectListScreen } from "./screens/project-list";
-import { TsReactTest } from "./try-use-array";
+import { LoginScreen } from "./screens/login";
 
 function App() {
   return (
     <div className="App">
-      <TsReactTest />
+      <LoginScreen />
       {/*<ProjectListScreen />*/}
     </div>
   );
