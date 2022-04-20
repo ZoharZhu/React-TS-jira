200d4643785dac39c98a68361447b37d79008081
Zzh-Zh3nDu1 <530178528@qq.com>
2022-04-20 16:20:14 +0800

    feat: 添加登录状态维持

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -2,9 +2,7 @@ import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { useEffect, useState } from "react";
 import { cleanObject, useMount, useDebounce } from "../../utils";
-import * as qs from "qs";
-
-const apiUrl = process.env.REACT_APP_API_URL;
+import { useHttp } from "../../utils/http";
 
 export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
@@ -14,23 +12,14 @@ export const ProjectListScreen = () => {
   });
   const [list, setList] = useState([]);
   const debouncedParam = useDebounce(param, 500);
+  const client = useHttp();
 
   useEffect(() => {
-    fetch(
-      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
   }, [debouncedParam]);
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client("users").then(setUsers);
   });
 
   return (
---
+++ src/utils/http.ts
@@ -0,0 +1,52 @@
+import qs from "qs";
+import * as auth from "../auth-provider";
+import { useAuth } from "../context/auth-context";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+interface Config extends RequestInit {
+  token?: string;
+  data?: object;
+}
+export const http = async (
+  endpoint: string,
+  { data, token, headers, ...customConfig }: Config = {}
+) => {
+  const config = {
+    method: "GET", // 默认为GET 可能被customConfig覆盖
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+      "Content-Type": data ? "application/json" : "",
+    },
+    ...customConfig,
+  };
+  // fetchapi GET方法所传参数放在url里 POST PATCH DELETE放在body里
+  if (config.method.toUpperCase() === "GET") {
+    endpoint += `?${qs.stringify(data)}`;
+  } else {
+    config.body = JSON.stringify(data || {});
+  }
+
+  // axios 和 fetch 的表现不一样 axios可以直接在返回状态不为2xx时抛出异常
+  return window
+    .fetch(`${apiUrl}/${endpoint}`, config)
+    .then(async (response) => {
+      if (response.status === 401) {
+        await auth.logout();
+        window.location.reload();
+        return Promise.reject({ message: "请重新登录" });
+      }
+      const data = await response.json();
+      if (response.ok) {
+        return data;
+      } else {
+        return Promise.reject(data); // fetchapi在服务端报错时不会自动抛出异常
+      }
+    });
+};
+
+export const useHttp = () => {
+  const { user } = useAuth();
+  return (...[endpoint, config]: Parameters<typeof http>) =>
+    http(endpoint, { ...config, token: user?.token });
+  // 等同于[endpoint, config]: [string, Config]
+};
