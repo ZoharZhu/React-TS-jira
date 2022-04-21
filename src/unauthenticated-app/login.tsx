be34d8853af1bdf71c5c815503bd9a4ea087beb4
Zzh-Zh3nDu1 <530178528@qq.com>
2022-04-21 21:03:04 +0800

    style: 安装emotion，优化登录注册界面

--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -1,5 +1,6 @@
 import { useAuth } from "../context/auth-context";
-import { Button, Form, Input } from "antd";
+import { Form, Input } from "antd";
+import { LongButton } from "./index";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -26,9 +27,9 @@ export const LoginScreen = () => {
         <Input placeholder="密码" type="password" id="password" />
       </Form.Item>
       <Form.Item>
-        <Button htmlType="submit" type="primary">
+        <LongButton htmlType="submit" type="primary">
           登录
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,6 +1,6 @@
-import { FormEvent } from "react";
 import { useAuth } from "../context/auth-context";
-import { Button, Form, Input } from "antd";
+import { Form, Input } from "antd";
+import { LongButton } from "./index";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -27,9 +27,9 @@ export const RegisterScreen = () => {
         <Input placeholder="密码" type="password" id="password" />
       </Form.Item>
       <Form.Item>
-        <Button htmlType="submit" type="primary">
+        <LongButton htmlType="submit" type="primary">
           注册
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
