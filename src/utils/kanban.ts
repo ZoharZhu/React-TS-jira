38894eee8e47f22cb9fb2998a29d6a681067c467
Zzh-Zh3nDu1 <530178528@qq.com>
2022-05-11 11:13:16 +0800

    feat: 添加看板和任务

--- src/utils/kanban.ts
+++ src/utils/kanban.ts
@@ -1,6 +1,7 @@
-import { useQuery } from "react-query";
+import { QueryKey, useMutation, useQuery } from "react-query";
 import { Kanban } from "../types/kanban";
 import { useHttp } from "./http";
+import { useAddConfig } from "./use-optimistic-options";
 
 // 获取看板列表
 export const useKanbans = (param?: Partial<Kanban>) => {
@@ -9,5 +10,19 @@ export const useKanbans = (param?: Partial<Kanban>) => {
   return useQuery<Kanban[]>(
     ["kanbans", param], // 第一个是key 第二个代表依赖 param改变 useQuery就重新获取一次
     () => client("kanbans", { data: param })
+  );
+};
+
+// 添加看板
+export const useAddKanban = (queryKey: QueryKey) => {
+  const client = useHttp();
+
+  return useMutation(
+    (params: Partial<Kanban>) =>
+      client(`kanbans`, {
+        method: "POST",
+        data: params,
+      }),
+    useAddConfig(queryKey)
   );
 };
--- src/utils/task.ts
+++ src/utils/task.ts
@@ -1,6 +1,7 @@
-import { useQuery } from "react-query";
+import { QueryKey, useMutation, useQuery } from "react-query";
 import { Task } from "../types/task";
 import { useHttp } from "./http";
+import { useAddConfig } from "./use-optimistic-options";
 
 // 获取任务列表
 export const useTasks = (param?: Partial<Task>) => {
@@ -9,5 +10,19 @@ export const useTasks = (param?: Partial<Task>) => {
   return useQuery<Task[]>(
     ["tasks", param], // 第一个是key 第二个代表依赖 param改变 useQuery就重新获取一次
     () => client("tasks", { data: param })
+  );
+};
+
+// 添加任务
+export const useAddTask = (queryKey: QueryKey) => {
+  const client = useHttp();
+
+  return useMutation(
+    (params: Partial<Task>) =>
+      client(`tasks`, {
+        method: "POST",
+        data: params,
+      }),
+    useAddConfig(queryKey)
   );
 };
